dda30c8f7978e18bf6973e43e059af0ffc47c296
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2024-05-24 12:52:54 +0200

    asd
    asd

--- matrix_operations/Matrix.py
+++ matrix_operations/Matrix.py
@@ -1,8 +1,12 @@
-class Matrix:
-  def __init__(self, values):
+import itertools
 
-    self.col = len(values)
-    self.row = len(values[0])
+
+class Matrix:
+
+  def __init__(self, values):
+    import itertools
+    self.row = len(values)
+    self.col = len(values[0])
     self.values = values
 
   def add(self, x, y):
@@ -10,7 +14,7 @@ class Matrix:
 
   def __add__(self, other):
     if ((self.col == other.col) and (self.row == other.row)):
-      return list(map(self.add,self.values, other.values))
+      return list(map(self.add,itertools.chain.from_iterable(self.values), itertools.chain.from_iterable(other.values)))
 
   def __mul__(self, other):
     pass
--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,15 +1,13 @@
 from Matrix import Matrix
 
+
 with open('./input.txt', 'r') as f:
   input = f.read()
 #print(input)
 
 
-m1 = [[1,2,3],
-      [3,4,3]]
-
-m2 = [[5,6,4],
-      [7,8,5]]
+m1 = [[1,2],[3,4]]
+m2 = [[5,6],[7,8]]
 
 A = Matrix(m1)
 B = Matrix(m2)

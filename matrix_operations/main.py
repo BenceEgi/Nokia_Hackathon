1b56aff621454bbb0927f240ada921fb469f29f1
Amongúz <120624900+BenceEgi@users.noreply.github.com>
2024-05-24 10:23:11 +0200

    F1

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,4 +1,13 @@
 with open('./input.txt', 'r') as f:
   input = f.read()
-
 print(input)
+
+class Matrix:
+  def __init__(self, width, height, values):
+    pass
+
+  def __add__(self, other):
+    pass
+
+  def __mul__(self, other):
+    pass
--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,13 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+import re
 
-print(input)
+with open('./input.txt', 'r') as f:
+
+  for input in f:
+
+    input_formatted = re.sub("[^a-zA-Z0-9]", "", input).lower().strip()
+
+    if (input_formatted == input_formatted[::-1]):
+      print(f"YES, {len(set(input_formatted))}")
+
+    else:
+      print("NO, -1")

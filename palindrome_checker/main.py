918f0fc338c114b015832e3b9e841c37699b0802
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2024-05-24 15:17:58 +0200

    asd
    ad

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -2,12 +2,14 @@ from re import sub
 
 with open('./input.txt', 'r') as f:
 
-  for input in f:
+    for i in f:
 
-    input = sub("[^a-zA-Z0-9]", "", input).strip().lower()
+        input_formatted = sub("[^a-zA-Z0-9]", "", i).strip().lower()
+        print(input_formatted)
 
-    if (input == input[::-1]):
-      print(f"YES, {len(set(input))}")
+        if input_formatted == input_formatted[::-1]:
+            print(f"YES, {len(set(input_formatted))}")
 
-    else:
-      print("NO, -1")
+        else:
+            print("NO, -1")
+

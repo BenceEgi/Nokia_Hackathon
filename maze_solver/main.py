139f8106bdcc67224048ed806d0c809e18eddcb6
Égi-Matus Bence <120624900+BenceEgi@users.noreply.github.com>
2024-05-25 10:27:31 +0200

    maze 1.0

--- maze_solver/main.py
+++ maze_solver/main.py
@@ -1,4 +1,17 @@
+import time
+
+start = time.time()
 with open('./input.txt', 'r') as f:
   input = f.read()
+  input = input.split("\n")
+  A = str(input[input.index("A")+1:input.index("")])[1:-1].replace(",","").replace("'", '').split(" ")
+  input.remove("")
+  B = str(input[input.index("B")+1:input.index("")])[1:-1].replace(",","").replace("'", '').split(" ")
+  input.remove("")
+  C = str(input[input.index("C")+1:input.index("")])[1:-1].replace(",","").replace("'", '').split(" ")
 
-print(input)
+print(A)
+print(B)
+print(C)
+
+print(time.time() - start)
